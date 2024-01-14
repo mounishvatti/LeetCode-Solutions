7084cc6655029e81a53c6e309695dead2450e3e9
mounishvatti <mounishvatti2002@gmail.com>
2024-01-14 17:57:26 +0530

    new ques

---
+++ inttoroman.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string intToRoman(int num) {
+        string roman = "";
+        vector<pair<int, string>> data = {{1000,"M"},{900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
+
+        for(int i=0; i<data.size(); i++){
+            while(num>=data[i].first){
+                roman+=data[i].second;
+                num-=data[i].first;
+            }
+        }
+        return roman;
+    }
+};
